1e3b9e79e106043a5f501f30c3f0eb63de3906ed
Akhil Bondlela <akhil.bondlela@gmail.com>
2017-10-14 22:31:05 -0400

    Faster CV

--- face_test.py
+++ face_test.py
@@ -30,65 +30,67 @@ face_names = []
 process_this_frame = True
 
 while True:
-    print(count)
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
     count = count + 1
-    if(count == 60):
+    if(count == 360):
         count = 0
 
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
+    face_locations = face_recognition.face_locations(small_frame)
     # Only process every other frame of video to save time
-    if process_this_frame:
+    if (count % 30 == 0):
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(small_frame)
         face_encodings = face_recognition.face_encodings(small_frame, face_locations)
 
         face_names = []
-        if (count % 10 == 0):
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                match = face_recognition.compare_faces(
-                    [obama_face_encoding, nick_face_encoding], face_encoding)
-                name = "Unknown"
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            match = face_recognition.compare_faces(
+                [obama_face_encoding, nick_face_encoding], face_encoding)
+            name = "Unknown"
 
-                if match[0]:
-                    name = "Obama"
+            if match[0]:
+                name = "Obama"
 
-                if match[1]:
-                    name = "Nick"
+            if match[1]:
+                name = "Nick"
 
-                face_names.append(name)
+            face_names.append(name)
 
-        process_this_frame = not process_this_frame
+        # process_this_frame = not process_this_frame
 
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
 
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35),
-                          (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6),
-                        font, 1.0, (255, 255, 255), 1)
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35),
+                      (right, bottom), (0, 0, 255), cv2.FILLED)
+        # Draw a label with a name below the face
+        if(name != "Unknown"):
+            cv2.rectangle(frame, (left, bottom),
+                          (right, bottom + 35), (255, 255, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6),
+                    font, 1.0, (255, 255, 255), 1)
 
-        # Display the resulting image
-        cv2.imshow('Video', frame)
+    # Display the resulting image
+    cv2.imshow('Video', frame)
 
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # Release handle to the webcam
 video_capture.release()
